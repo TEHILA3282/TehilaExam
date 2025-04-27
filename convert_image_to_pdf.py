92a1b4fb20aa79941b93e89a060098f7952b2d21
Tehila <TE3282@GMAIL.COM>
2025-04-27 17:32:19 +0300

    Added README.md, .gitignore, and Docker run command

--- convert_image_to_pdf.py
+++ convert_image_to_pdf.py
@@ -9,16 +9,21 @@ if os.path.isdir(filepath):
     with open(f"output/{pdfname}.pdf", "wb") as f:
         imgs = []
         for fname in os.listdir(filepath):
-            if not fname.endswith(".JPG"):
+            if not fname.lower().endswith((".jpg", ".jpeg", ".png")):
                 continue
             path = os.path.join(filepath, fname)
             if os.path.isdir(path):
                 continue
             imgs.append(path)
-        f.write(img2pdf.convert(imgs))
+        if imgs:
+            f.write(img2pdf.convert(imgs))
+        else:
+            print("No valid images found!")
 elif os.path.isfile(filepath):
-    if filepath.endswith(".jpg"):
+    if filepath.lower().endswith((".jpg", ".jpeg", ".png")):
         with open(f"output/{pdfname}.pdf", "wb") as f:
             f.write(img2pdf.convert(filepath))
+    else:
+        print("Only image files (.jpg/.jpeg/.png) are supported!")
 else:
     print("please input file or dir")
